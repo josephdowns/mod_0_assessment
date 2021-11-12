a5890edbd47d00cb0c5970db789ca67fcbc109d9
Joseph Downs <jos.w.downs@gmail.com>
2021-11-11 21:41:48 -0700

    Add laptop model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+laptop = {
+  make: "Apple",
+  model: "MacBook Pro",
+  year: 2020,
+  screen_size: 13.0,
+  is_purple: false,
+  is_cool: true,
+  fav_websites: ["Reddit", "Youtube", "Amazon"],
+  browsers: ["Chrome", "Safari", "Firefox"]
+}

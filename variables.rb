022455c437befeec8723511905db5fea065284dc
Joseph Downs <jos.w.downs@gmail.com>
2021-11-11 21:32:53 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+animal = "lion"
+is_cuddly = false
+animal = "octopus"
+
+num_8 = 4
+num_3 = 24
+
+quotient = num_3 / num_8
+
+favorite_films = ["Shortbus", "Stardust", "Dark Crystal"]
+
+favorite_films.shift
+
+puts animal.length

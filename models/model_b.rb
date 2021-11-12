390d337a24f32f9633051cd31a5f2afce407996c
Joseph Downs <jos.w.downs@gmail.com>
2021-11-11 21:48:28 -0700

    Add house model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+house = {
+  street: "Newton",
+  color: "red",
+  floors: 2,
+  number_of_bathrooms: 1.5,
+  is_old: true,
+  is_warm: false,
+  rooms: ["kitchen", "den", "study"],
+  styles: ["mid-century modern", "gothic", "classical"]
+}
